e72097d29608f4109ea4c7f631abb88fde6f3f42
Tony380 <tonygon2@yahoo.es>
2020-11-12 12:30:13 -0300

    travis.py

---
+++ france/travis.py
@@ -0,0 +1,13 @@
+"""Travis settings"""
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
